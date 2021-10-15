5c14857e1591ff0bc7920c9f9db8c9582b929143
pratham022 <pthombre657@gmail.com>
2021-10-15 17:07:27 +0530

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter} from 'react-router-dom'
+import Routing from './Routing';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: "#FAE8E0" }}>
+      <BrowserRouter>
+        <Routing/>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Routing.js
@@ -0,0 +1,10 @@
+import Navbar from "./components/Navbar";
+import './Background/StarryNight.css';
+
+function Routing() {
+    return (
+         <Navbar style={{position:"fixed"}}></Navbar> 
+    );
+}
+  
+  export default Routing;
